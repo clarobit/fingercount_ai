e3599ed7c94ae88599893393597eb73b727718fc
goodboy-kor <llaayy.kr@gmail.com>
2026-01-19 14:20:18 +0900

    uart python add

---
+++ python/uart/uart.py
@@ -0,0 +1,20 @@
+# import serial
+
+# PORT = "/dev/ttyACM0"
+# # BAUD = 115200
+# BAUD = 921600
+
+
+# ser = serial.Serial(PORT, BAUD, timeout=1)
+
+# print("Listening...")
+
+# while True:
+#     line = ser.readline()
+#     if line:
+#         try:
+#             print(line.decode(errors="ignore").strip())
+#         except Exception as e:
+#             print("decode error:", e)
+
+
